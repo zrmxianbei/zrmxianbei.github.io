934950b66cf23b08d14738966c9b4de9f05e63fa
zrmxianbei <86840893+zrmxianbei@users.noreply.github.com>
2025-02-10 12:57:56 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 14100;
+
+app.get('/data', (req, res) => {
+  const data = {
+    message: 'Hello, this is your data!',
+    items: [1, 2, 3, 4, 5]
+  };
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://49.235.162.94:${port}`);
+});
